c50a677b6318d5adcdc9537b9ece977e2f870659
unknown <dipanjana.ghosh@bandhan.org>
2023-11-09 21:07:33 +0530

    Add Header and Left Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,7 +18,8 @@ import { DataCardsComponent } from './components/data-cards/data-cards.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxBootstrapIconsModule.pick(allIcons)
   ],
   providers: [],
   bootstrap: [AppComponent]
